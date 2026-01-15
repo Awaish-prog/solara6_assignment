cba45d9e6a8c369847229a96fec0d357cfb71642
Awaish_prog <awaishk640@gmail.com>
2026-01-15 06:12:05 +0530

    added default env vars

--- src/support/env.ts
+++ src/support/env.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// added defaults in case someone doesn't want to add .env, should not be done in real world projects
 export const env = {
   validUser: {
-    email: process.env.VALID_USER_EMAIL!,
-    password: process.env.VALID_USER_PASSWORD!,
+    email: process.env.VALID_USER_EMAIL ?? 'test@awaish.com',
+    password: process.env.VALID_USER_PASSWORD ?? 'xyzxyzxyz',
   },
-  baseUrl: process.env.BASE_URL,
+  baseUrl: process.env.BASE_URL ?? 'https://demowebshop.tricentis.com',
   headless: !process.env.HEADLESS || process.env.HEADLESS !== 'false'
 };
